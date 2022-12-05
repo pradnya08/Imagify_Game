d15f282d8d7219ecd588cab5541ed9d9bebf136f
pradnya08 <pradnya.asundi@gmail.com>
2022-12-04 19:16:44 -0600

    Made changes to the how to play instructions

--- src/pages/HowToPlay.jsx
+++ src/pages/HowToPlay.jsx
@@ -11,8 +11,12 @@ export default function HowToPlay() {
 				</h1>
 			</div>
 			<div className='flex justify-center flex-col items-center'>
-				<p className='m-4 pt-10 text-center'>This is how we play the game:</p>
 				<p className='w-1/2 font-serif mb-8'>
+					Image cleaning is a very important and crucial part in visual data
+					analytics and by playing this game you will be helping us collect data
+					about clarity of the image and we can use this data to train our
+					models to better understand visual data.
+					<br />
 					You will be shown an image along with three options below the image{" "}
 					<b>Clear, Blur</b> or <b>Not Sure</b>
 					<br />
@@ -25,11 +29,6 @@ export default function HowToPlay() {
 					Once you have made your choice click <b>Next Image</b> button to get a
 					new image. Your performance will be scored and based on what you
 					choose for the images.
-					<br />
-					Image cleaning is a very important and crucial part in visual data
-					analytics and by playing this game you will be helping us collect data
-					about clarity of the image and we can use this data to train our
-					models to better understand visual data.
 					<br />
 					You will level up based on the scores and you can see how you stand
 					with other players by checking the Leaderboard page.
